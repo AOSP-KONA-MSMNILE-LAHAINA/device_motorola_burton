40372f09f24636bfe0c29294b6d4f3318682720c
Akash Srivastava <akashniki@gmail.com>
2024-03-01 23:02:48 +0100

    burton: Import framework compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,10 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml
 
