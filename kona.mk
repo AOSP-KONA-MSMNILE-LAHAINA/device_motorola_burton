90e9e7fd9439a85546806f98fe4e8f56020d660d
Akash Srivastava <akashniki@gmail.com>
2024-03-02 01:28:10 +0100

    burton: audio: Import stock audio_policy_configuration

--- kona.mk
+++ kona.mk
@@ -44,6 +44,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Authsecret
