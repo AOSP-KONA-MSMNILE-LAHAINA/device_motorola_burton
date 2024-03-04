36e27c354e22da13bf1a08ff4a7aa5fe4f1840ab
Akash Srivastava <akashniki@gmail.com>
2024-03-04 10:01:55 +0100

    burton: Pixel Experience bringup

---
+++ aosp_burton.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 PixelExperience
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, device/motorola/burton/kona.mk)
+
+# Inherit from the Lineage configuration.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Pixel Experience additions
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_BRAND := Motorola
+PRODUCT_DEVICE := burton
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_MODEL := Motorola Edge Plus
+PRODUCT_NAME := aosp_burton
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
